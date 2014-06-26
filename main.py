c8a40004e7f9e2496479d24f34887a8fc2407aa7
ant <twinsant@gmail.com>
2014-06-26 19:07:53 +0800

    Add: Redis cache

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import hashlib
 
 from tornado.web import RequestHandler
 from tornado.web import URLSpec
@@ -14,6 +15,10 @@ from tornado.httpclient import HTTPError
 from tornado.web import asynchronous
 from tornado import gen
 
+import redis
+
+CACHE_TIME = 60
+
 http_client = AsyncHTTPClient()
 
 class IndexHandler(RequestHandler):
@@ -24,24 +29,47 @@ class ProxyHandler(RequestHandler):
     @asynchronous
     @gen.engine
     def get(self):
+        referer = self.request.headers.get('Referer')
+        logging.debug('TODO: check referer %s' % referer)
         maps = {
             'fonts.twinsant.com': 'fonts.googleapis.com',
             'themes.twinsant.com': 'themes.googleusercontent.com',
         }
         url = 'http://%s%s' % (maps[self.request.host], self.request.uri)
-        logging.debug('Fetching %s ...' % url)
-        response = yield gen.Task(http_client.fetch, url, connect_timeout=60.0, request_timeout=60.0)
-        if response.code == 200:
-            content_type = response.headers.get('Content-Type')
-            if self.request.host == 'fonts.twinsant.com':
-                html = response.body.replace('http://themes.googleusercontent.com', 'http://themes.twinsant.com')
-                self.write(html)
-            if self.request.host == 'themes.twinsant.com':
-                self.write(response.body)
-            self.set_header('Content-Type', content_type)
-        else:
-            raise HTTPError(response.code)
+
+        @gen.engine
+        def fetch_content(url, callback=None):
+            key = hashlib.md5(url).hexdigest()
+            content_type = self.application.redis.get(self.get_key(key, 'content_type'))
+            content = self.application.redis.get(self.get_key(key, 'content'))
+            if content_type is None or content is None:
+                logging.debug('%s: Fetching %s ...' % (key, url))
+                response = yield gen.Task(http_client.fetch, url, connect_timeout=60.0, request_timeout=60.0)
+                if response.code == 200:
+                    content_type = response.headers.get('Content-Type')
+                    self.application.redis.set(self.get_key(key, 'content_type'), content_type)
+                    self.application.redis.expire(self.get_key(key, 'content_type'), CACHE_TIME)
+                    self.application.redis.set(self.get_key(key, 'content'), response.body)
+                    self.application.redis.expire(self.get_key(key, 'content'), CACHE_TIME)
+                    callback(response.body, content_type)
+                else:
+                    raise HTTPError(response.code)
+            else:
+                callback(content, content_type)
+
+        ret, foo = yield gen.Task(fetch_content, url)
+        content = ret[0]
+        content_type = ret[1]
+        if self.request.host == 'fonts.twinsant.com':
+            html = content.replace('http://themes.googleusercontent.com', 'http://themes.twinsant.com')
+            self.write(html)
+        if self.request.host == 'themes.twinsant.com':
+            self.write(content)
+        self.set_header('Content-Type', content_type)
         self.finish()
+
+    def get_key(self, key, name):
+        return 'hongniang:%s:%s' % (key, name)
 
 handlers = [
     URLSpec('/', IndexHandler),
@@ -56,10 +84,16 @@ def main():
     define('debug', default=False, help='debug', type=bool)
     define('port', default=1900, help='port', type=int)
 
+    define('rds_host', default='127.0.0.1', help='Redis host')
+    define('rds_port', default=6379, help='Redis port', type=int)
+    define('rds_db', default=0, help='Redis database', type=int)
+
     parse_command_line()
     settings['debug'] = options.debug
 
     application = Application(handlers, **settings)
+
+    application.redis = redis.StrictRedis(host=options.rds_host, port=options.rds_port, db=options.rds_db)
     http_server = HTTPServer(application, xheaders=True)
     http_server.listen(options.port)
     logging.info('Listen on %d, debug=%s' % (options.port, options.debug))
